2757fb27b6defeb44ef722443100d1db758d629c
Ben Lewis <ben@karmascience.com>
2013-07-26 17:24:19 -0700

    Super pretty

--- app/assets/javascripts/tip.js
+++ app/assets/javascripts/tip.js
@@ -26,4 +26,58 @@ $(function(){
     }
   }
 
+  $('.dollar')
+
+
+  $('#other-input').keyup(function() {
+    var hidden  = $('#charge-amount');
+    hidden.val($(this).val() * 100).trigger('change');
+  }).blur(function() {
+    var hidden  = $('#charge-amount');
+    hidden.val($(this).val() * 100).trigger('change');
+    $(this).formatCurrency();
+    $(this).formatCurrency('.currencyLabel');
+  }).focus(function () {
+    $(this).val('');
+  });
+
+  $('#charge-amount').change(function() {
+    console.log('Charge amount changed to ' + $(this).val());
+  });
+
+  $('#charge-amount-selector').children('a').each(function(){
+    var button  = $(this);
+    var group   = button.parents('#charge-amount-selector');
+    var form    = button.parents('form').eq(0);
+    var name    = group.attr('data-toggle-name');
+    var hidden  = $('input[name="' + name + '"]', form);
+    var value   = $(button).attr('data-value');
+
+    button.click(function() {
+      console.log('Clicked ' + value);
+      hidden.val(value).trigger('change');
+
+      group.children('a').each(function() {
+        if (hidden.val() == $(this).attr('data-value')) {
+          button.addClass('active');
+        } else {
+          $(this).removeClass('active');
+        }
+      })
+
+      input = $('#other-input');
+      if (value.indexOf("-") != -1) { //Other
+        parts = value.split('-');
+        input.attr('min', parts[0]);
+        input.val(parts[1] / 100); //default
+        input.attr('max', parts[2]);
+        input.formatCurrency();
+        input.formatCurrency('.currencyLabel');
+        input.css('display', 'inline');
+      } else {
+        input.css('display', 'none');
+      }
+    });
+
+  });
 });
